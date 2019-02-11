bc20c0df8029a4e7f745c91bf609c5aecbef45a6
Riley Brown <brown.riley.c@gmail.com>
2019-02-11 14:21:33 -0500

    added dotenv and finished functions in the manager version of the app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-
+require('dotenv').config();
 
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Horsesrunfast7',
+    password: process.env.DB_Password,
     database: 'bamazon_db'
 });
 
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
-
+require('dotenv').config();
 
 var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Horsesrunfast7',
+    password: process.env.DB_Password,
     database: 'bamazon_db'
 });
 
@@ -30,6 +30,7 @@ connection.connect(function (err) {
                 addToInventory();
                 break;
             case 'Add new product':
+                addNewProduct();
                 break;
             default:
                 console.log('What did you do?');
@@ -37,10 +38,49 @@ connection.connect(function (err) {
     })
 });
 
+function addNewProduct() {
+    inquirer.prompt(
+        [
+            {
+                name: 'product_name',
+                type: 'input',
+                message: 'What item is being added to the inventory?'
+            },
+            {
+                name: 'product_dept',
+                type: 'input',
+                message: 'What department does it belong to?'
+            },
+            {
+                message: 'What is the price for the item?',
+                type: 'input',
+                name: 'product_price',
+                validate: function (name) {
+                    return !isNaN(name);
+                }
+            },
+            {
+                message: 'How many of the item are being added to the inventory?',
+                type: 'input',
+                name: 'product_quantity',
+                validate: function (name) {
+                    return !isNaN(name);
+                }
+            }
+        ]).then(function (response) {
+            let insert = "INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ('"+response.product_name + "', '" + response.product_dept + "', " + parseFloat(response.product_price) + ", " + parseInt(response.product_quantity) + ")";
+            console.log(insert);
+            connection.query(insert, function (err, response) {
+                if (err) throw err;
+                readProducts();
+            });
+        });
+}
+
 function addToInventory() {
-    connection.query("SELECT * FROM products", function (err, response) {
-        for (var i = 0; i < response.length; i++) {
-            console.log(response[i].item_id + " | " + response[i].product_name + " | " + response[i].department_name + " | " + response[i].price + " | " + response[i].stock_quantity);
+    connection.query("SELECT * FROM products", function (err, data) {
+        for (var i = 0; i < data.length; i++) {
+            console.log(data[i].item_id + " | " + data[i].product_name + " | " + data[i].department_name + " | " + data[i].price + " | " + data[i].stock_quantity);
         };
         console.log('==========================================')
         inquirer.prompt([
@@ -56,14 +96,15 @@ function addToInventory() {
             }
         ]).then(function (response) {
             connection.query('UPDATE products SET ? WHERE ?', [{
-                stock_quantity: (this.stock_quantity + parseInt(response.amount))
+                stock_quantity: (data[response.itemID-1].stock_quantity + parseInt(response.amount))
             },
             {
                 item_id: response.itemID
             }], function (err, data) {
                 if (err) throw err;
+                console.log('Here is the updated inventory\r\n================================')
+                readProducts();
             });
-            connection.end();
         })
     })
 };
